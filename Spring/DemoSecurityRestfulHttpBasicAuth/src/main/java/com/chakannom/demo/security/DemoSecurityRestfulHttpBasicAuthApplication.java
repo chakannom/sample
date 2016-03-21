7e6b19e262dbae88f17b6dbae73353625b991247
genteelson <genteelson@gmail.com>
2016-03-22 00:14:42 +0900

    DemoSecurityRestfulHttpBasicAuth init.

---
+++ Spring/DemoSecurityRestfulHttpBasicAuth/src/main/java/com/chakannom/demo/security/DemoSecurityRestfulHttpBasicAuthApplication.java
@@ -0,0 +1,12 @@
+package com.chakannom.demo.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityRestfulHttpBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityRestfulHttpBasicAuthApplication.class, args);
+	}
+}
